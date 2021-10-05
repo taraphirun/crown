b37ed1208be0d2449cf141b9f19fd1921c085aa5
taraphirun <taraphirun@live.com>
2021-10-05 23:55:18 +0700

    done

--- src/main.js
+++ src/main.js
@@ -1,21 +1,11 @@
 import Vue from 'vue';
 import vueCustomElement from 'vue-custom-element';
-import vuetify from '@/plugins/vuetify'; // path to vuetify export
-
-// import App from './App.vue';
-import StandaloneText from './components/StandaloneText.vue';
-import StyleModuleExample from './components/StyleModuleExample.vue';
-import VuetifyExample from './components/VuetifyExample.vue';
+import CrownNewsAddOn from './components/CrownNewsAddOn.vue';
 
 Vue.config.productionTip = false;
-
-// new Vue({
-//   render: (h) => h(App),
-// }).$mount('#app');
-
 Vue.use(vueCustomElement);
 
-Vue.customElement('standalone-text', StandaloneText, {
+Vue.customElement('crown-news-add-on', CrownNewsAddOn, {
   shadow: true,
   beforeCreateVueInstance(root) {
     const rootNode = root.el.getRootNode();
@@ -30,41 +20,6 @@ Vue.customElement('standalone-text', StandaloneText, {
       root.shadowRoot = document.head;
     }
 
-    return root;
-  },
-});
-
-Vue.customElement('style-module-example', StyleModuleExample, {
-  shadow: true,
-  beforeCreateVueInstance(root) {
-    const rootNode = root.el.getRootNode();
-
-    if (rootNode instanceof ShadowRoot) {
-      // eslint-disable-next-line no-param-reassign
-      root.shadowRoot = rootNode;
-    } else {
-      // eslint-disable-next-line no-param-reassign
-      root.shadowRoot = document.head;
-    }
-    return root;
-  },
-});
-
-VuetifyExample.vuetify = vuetify;
-
-Vue.customElement('vuetify-example', VuetifyExample, {
-  shadow: true,
-  // shadowCss: vuetifyCss,
-  beforeCreateVueInstance(root) {
-    const rootNode = root.el.getRootNode();
-
-    if (rootNode instanceof ShadowRoot) {
-      // eslint-disable-next-line no-param-reassign
-      root.shadowRoot = rootNode;
-    } else {
-      // eslint-disable-next-line no-param-reassign
-      root.shadowRoot = document.head;
-    }
     return root;
   },
 });
